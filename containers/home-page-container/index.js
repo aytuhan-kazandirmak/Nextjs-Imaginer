05994f7c580e33105e4397860ddafd5de950b48b
Aytuhan Kazandirmak <aytuhann@gmail.com>
2024-02-26 18:41:27 +0300

    created homepage container and its css file

---
+++ containers/home-page-container/index.js
@@ -0,0 +1,23 @@
+import styles from "./styles.module.scss";
+import PromptForm from "./prompt-form";
+import PromptSuggestions from "./prompt-suggestions";
+import Examples from "./examples";
+const HomePageContainer = () => {
+  return (
+    <div className={styles.homePageContainer}>
+      <h1 className={styles.title}>
+        You just imagine,
+        <br />
+        we handle the rest
+      </h1>
+      <p className={styles.description}>
+        Tell us a prompt and we&apos;ll generate a story for you.
+      </p>
+      <PromptForm />
+      <PromptSuggestions />
+      <Examples />
+    </div>
+  );
+};
+
+export default HomePageContainer;

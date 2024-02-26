d8bc34efa58d788a265221beebee832d2b37397f
Aytuhan Kazandirmak <aytuhann@gmail.com>
2024-02-26 18:41:46 +0300

    created tag component and its css file

---
+++ components/tag/index.js
@@ -0,0 +1,17 @@
+"use client";
+import styles from "./styles.module.scss";
+
+const Tag = ({ title, onClick }) => {
+  const handleClick = () => {
+    if (onClick) {
+      onClick(title);
+    }
+  };
+  return (
+    <button className={styles.tag} onClick={handleClick}>
+      {title}
+    </button>
+  );
+};
+
+export default Tag;
